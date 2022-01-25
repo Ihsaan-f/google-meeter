6ed15d1b996ba60d1572cd0005afd2a8c573ff7b
Ihsaan-f <85830250+Ihsaan-f@users.noreply.github.com>
2022-01-25 15:48:09 +0530

    Update main_google_meeter.py

--- main_google_meeter.py
+++ main_google_meeter.py
@@ -3,13 +3,21 @@ import pyautogui
 from google_meeter_back_end import meet1,meet2,meet3,login,meet4,meet5
 import time
 from selenium import webdriver
-time.sleep(5)
+time.sleep(2)
+
+#Gave idea and Example Code Kallil6454
+Time1 =str(input("Enter your Time for 1st meeting. 24 HOUR FORMAT:"))
+Time2 =str(input("Enter your Time for 4th meeting. 24 HOUR FORMAT:"))
+Time3 =str(input("Enter your Time for 3rd meeting. 24 HOUR FORMAT:"))
+Time4 =str(input("Enter your Time for 4th meeting. 24 HOUR FORMAT:"))
+Time5 =str(input("Enter your Time for 5th meeting. 24 HOUR FORMAT:"))
+
 #You can change the time of these 5 lines
-schedule.every().day.at("08:30").do(meet1)
-schedule.every().day.at("09:30").do(meet2)
-schedule.every().day.at("10:25").do(meet3)
-schedule.every().day.at("11:20").do(meet4)
-schedule.every().day.at("12:15").do(meet5)
+schedule.every().day.at(Time1).do(meet1)
+schedule.every().day.at(Time2).do(meet2)
+schedule.every().day.at(Time3).do(meet3)
+schedule.every().day.at(Time4).do(meet4)
+schedule.every().day.at(Time5).do(meet5)
 
 
 while 1:
