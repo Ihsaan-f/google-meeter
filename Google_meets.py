077dad9ca7b8f7da63c4d1c4b7c56d4aa850a89b
Ihsaan-f <85830250+Ihsaan-f@users.noreply.github.com>
2022-01-25 14:05:35 +0530

    Create Google_meets.py

---
+++ Google_meets.py
@@ -0,0 +1,18 @@
+#Put the links It is in order-
+
+# Example :- meeting_1 = "https://meet.google.com/gft-ikif-zan" do it like this in these meetings
+
+#first link that activates
+meeting_1 =""
+
+#Second link that activates
+meeting_2 =""
+
+#Third link that activates
+meeting_3 =""
+
+#Fourth link that activates
+meeting_4 =""
+
+#Fifth link that activates
+meeting_5 =""
